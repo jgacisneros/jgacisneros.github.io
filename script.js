eeaa599994a679af17d52e2449f2605a192dc8ff
jgacisneros <jgacisneros@hotmail.com>
2024-10-04 10:06:40 -0700

    Fixing typo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function temperature(){
-    //To convert celcius to farenheit
+    //To convert celsius to farenheit
     //(CEL * 9/5) + 32
     var c = document.getElementById("celsius").value;
     var f = (c * 9/5) + 32
